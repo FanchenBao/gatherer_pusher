51cd171a50c749c5ce94e328c2b52a064b3cac23
FanchenBao <jonathonbao@gmail.com>
2019-10-22 13:55:24 -0400

    Add initial support for sending MQTT to shadow client on aws iot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from multiprocessing import Queue, JoinableQueue
 from collect_data import collect_data
-from db import initialize, insert_mult_rows
+
+# from db import initialize, insert_mult_rows
 from utility import make_db_insertable_data
 from child_process import start_command, start_child, kill_child, kill_cmd
 from time import sleep
@@ -9,7 +10,9 @@ import logging
 import logging.config
 import yaml
 import os
-import sys
+from upload_service import UploadService
+
+# import sys
 
 
 def command_line_parser():
@@ -61,30 +64,31 @@ def main(logger):
         pass
 
     SESS_DUR = args.session_duration  # monitoring session duration
-    DB = f"./database/{args.database}"  # database location
-    TABLE_NAME = args.table_name
-    CREATE_TABLE = f""" CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
-                                        probeId INTEGER PRIMARY KEY,
-                                        macAddress NVARCHAR(64),
-                                        isPhysical BOOLEAN,
-                                        isWifi BOOLEAN,
-                                        captureTime DATETIME,
-                                        rssi INTEGER,
-                                        channel INTEGER
-                                    ); """
+    # DB = f"./database/{args.database}"  # database location
+    # TABLE_NAME = args.table_name
+    # CREATE_TABLE = f""" CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
+    #                                     probeId INTEGER PRIMARY KEY,
+    #                                     macAddress NVARCHAR(64),
+    #                                     isPhysical BOOLEAN,
+    #                                     isWifi BOOLEAN,
+    #                                     captureTime DATETIME,
+    #                                     rssi INTEGER,
+    #                                     channel INTEGER
+    #                                 ); """
     # schema WITHOUT the autoincremented rowid (i.e. probeId in this context)
-    SCHEMA = "macAddress,isPhysical,isWifi,captureTime,rssi,channel"
+    # SCHEMA = "macAddress,isPhysical,isWifi,captureTime,rssi,channel"
     # main.py must be right outside `sniff-probes` folder.
-    CMD = f"sniff-probes/sniff-probes.sh -i {args.interface} --channel_hop"
+    CMD = f"./sniff-probes.sh -i {args.interface} --channel_hop"
     RETRY_INTERVAL: int = 10  # wait time before retry database connection or spinning up child processes
     TOTAL_RETRIES: int = 5  # Total number of retries allowed.
     wifi_data_q = Queue()  # transmit data from col_data_proc to here
     msg_q = JoinableQueue()  # inform health of child process
 
     # main loop
-    reinsert: bool = False  # flag, whether re-inserting row is needed.
-    conn = None  # database connection, default to None
+    # reinsert: bool = False  # flag, whether re-inserting row is needed.
+    # conn = None  # database connection, default to None
     start_process = True  # flag, whether child processes need to be spun up
+    us = UploadService()
     while True:
         if start_process:
             # start probing. Probing never stops
@@ -104,38 +108,49 @@ def main(logger):
             )
             start_process = False
 
-        if conn is None:  # initialize database
-            conn = initialize(DB, CREATE_TABLE, RETRY_INTERVAL, TOTAL_RETRIES)
-            # if db cannot be initiated after multiple retries, end program.
-            if not conn:
-                kill_cmd(probe_proc, "Probe Request Sniffing")
-                kill_child(col_data_proc, "Data Collection")
-                sys.exit(1)
+        # if conn is None:  # initialize database
+        #     conn = initialize(DB, CREATE_TABLE, RETRY_INTERVAL, TOTAL_RETRIES)
+        #     # if db cannot be initiated after multiple retries, end program.
+        #     if not conn:
+        #         kill_cmd(probe_proc, "Probe Request Sniffing")
+        #         kill_child(col_data_proc, "Data Collection")
+        #         sys.exit(1)
 
-        insertable = ""  # dummy, fix "local variable referenced before assign"
-        while conn and msg_q.empty():
-            # assign new insertable if there is no need to reinsert
-            if not reinsert and not wifi_data_q.empty():
-                insertable = make_db_insertable_data(wifi_data_q.get(), True)
-            # If error occurs during insertion, retry db connection and set
-            # `reinsert` to True
-            if not insert_mult_rows(conn, insertable, TABLE_NAME, SCHEMA):
-                reinsert = True
-                conn.close()
-                conn = None  # indicate error happens at database level
-                break
-            else:
-                reinsert = False
+        # insertable = ""  # dummy, fix "local variable referenced before assign"
+        # while conn and msg_q.empty():
+        #     # assign new insertable if there is no need to reinsert
+        #     if not reinsert and not wifi_data_q.empty():
+        #         insertable = make_db_insertable_data(wifi_data_q.get(), True)
+        #     # If error occurs during insertion, retry db connection and set
+        #     # `reinsert` to True
+        #     if not insert_mult_rows(conn, insertable, TABLE_NAME, SCHEMA):
+        #         reinsert = True
+        #         conn.close()
+        #         conn = None  # indicate error happens at database level
+        #         break
+        #     else:
+        #         reinsert = False
 
         # error in data collection or probing
+
+        # push data directly to cloud. Currently the pushing frequency is
+        # the same as the probing session duration
+        while msg_q.empty():
+            insertable = make_db_insertable_data(wifi_data_q.get(), True)
+            if insertable:
+                us.send_MQTT(insertable)
+            while insertable:
+                row_data = insertable.popleft()
+                print(row_data)
+
         if not msg_q.empty() and msg_q.get() == "fail":
             msg_q.task_done()
             kill_cmd(probe_proc, "Probe Request Sniffing")
             kill_child(col_data_proc, "Data Collection")
             logger.info("Retry probing and data collection in 10 seconds")
             start_process = True
-        if conn is None:  # error in database
-            logger.info("Reconnect to database in 10 seconds")
+        # if conn is None:  # error in database
+        #     logger.info("Reconnect to database in 10 seconds")
         sleep(10)
 
 
---
+++ upload_service.py
@@ -0,0 +1,66 @@
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+import aws_iot_config
+import json
+import time
+import logging
+import logging.config
+import yaml
+
+# set up logger
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f.read())
+    logging.config.dictConfig(config)
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+
+
+# Function called when a shadow is updated
+def customShadowCallback_Update(payload, responseStatus, token):
+    # Display status and data from update request
+    if responseStatus == "timeout":
+        logger.info("Update request " + token + " time out!")
+
+    if responseStatus == "accepted":
+        payloadDict = json.loads(payload)
+        logger.info("~~~~~~~~~~~~~~~~~~~~~~~")
+        logger.info("Update request with token: " + token + " accepted!")
+        logger.info(payloadDict)
+        logger.info("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+
+    if responseStatus == "rejected":
+        logger.error("Update request " + token + " rejected!")
+
+
+class UploadService:
+    """ A class to handle uploading probe request data via MQTT to aws IoT.
+    """
+
+    def __init__(self):
+        # Create, configure, and connect a shadow client.
+        self.myShadowClient = AWSIoTMQTTShadowClient(
+            aws_iot_config.SHADOW_CLIENT
+        )
+        self.myShadowClient.configureEndpoint(
+            aws_iot_config.HOST_NAME, aws_iot_config.PORT
+        )
+        self.myShadowClient.configureCredentials(
+            aws_iot_config.ROOT_CA,
+            aws_iot_config.PRIVATE_KEY,
+            aws_iot_config.CERT_FILE,
+        )
+        # AWSIoTMQTTShadowClient connection configuration
+        self.myShadowClient.configureAutoReconnectBackoffTime(1, 32, 20)
+        self.myShadowClient.configureConnectDisconnectTimeout(10)
+        self.myShadowClient.configureMQTTOperationTimeout(5)
+        self.myShadowClient.connect()
+
+        # Create a programmatic representation of the shadow.
+        self.myDeviceShadow = self.myShadowClient.createShadowHandlerWithName(
+            aws_iot_config.SHADOW_HANDLER, True
+        )
+
+    def send_MQTT(self, payload):
+        logger.debug("send_MQTT called")
+        self.myDeviceShadow.shadowUpdate(
+            json.dumps(payload), customShadowCallback_Update, 5
+        )
+        time.sleep(1)
